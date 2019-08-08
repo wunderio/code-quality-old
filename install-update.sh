9a74d1920f01c5ec9978824a9dfec7369ecd33c4
Ragnar Kurm <ragnar.kurm@wunder.io>
2019-08-08 17:41:55 +0300

    Add install-update.sh script

--- install-update.sh
+++ install-update.sh
@@ -1,1 +1,23 @@
 #!/bin/sh
+
+if [ ! -d '.git' ]; then
+  echo "Error: No git repo!"
+  exit 0
+fi
+
+if [ ! -d '.git/hooks' ]; then
+  echo "Error: No hooks in git repo!"
+  exit 0
+fi
+
+src=vendor/wunderio/code-quality/pre-commit
+if [ ! -f "$src" ]; then
+  echo "Error: missing $src"
+  exit 0
+fi
+
+dst=.git/hooks/pre-commit
+if [ ! -L "$dst" ]; then
+  rm -fv "$dst"
+  ln -sv "$src "$dst"
+fi
